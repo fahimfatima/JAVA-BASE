32da0058c785612b472ac112f0397aa28826496d
fahimfatima <fatimafahim.dev@gmail.com>
2022-02-18 19:15:30 +0000

    tp6

---
+++ src/ma/education/tp6/annotations/Calculatrice.java
@@ -0,0 +1,5 @@
+package ma.education.tp6.annotations;
+@Programmer(id=22,name="FATIMA FAHIM")
+
+public class Calculatrice {
+}
---
+++ src/ma/education/tp6/annotations/Programmer.java
@@ -0,0 +1,13 @@
+package ma.education.tp6.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Programmer {
+    abstract int id();
+    String name();
+
+}

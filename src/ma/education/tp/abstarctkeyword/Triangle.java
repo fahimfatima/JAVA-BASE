5117676cdff683d0ad6a5e6ff7bf372a9cf1ee13
Fatima-Fahim <96266519+fahimfatima@users.noreply.github.com>
2022-03-18 21:33:45 +0000

    Update Triangle.java

--- src/ma/education/tp/abstarctkeyword/Triangle.java
+++ src/ma/education/tp/abstarctkeyword/Triangle.java
@@ -1,4 +1,11 @@
 package ma.education.tp.abstarctkeyword;
 
 public abstract class Triangle extends FormeGeoTypeA{
+  int h;
+    int l ;
+    @Override
+    public double calculerSurface() {
+
+        return (h*l)/2;
+    }
 }
